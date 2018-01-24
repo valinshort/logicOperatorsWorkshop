5a14f43dea8560fb854cd59174e55ce753419cf2
Valin Short <valin.short1998@gmail.com>
2018-01-24 16:40:06 -0500

    logic operations

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,36 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+
+   int a =2;
+   int b = 4;
+   int x = 6;
+   int y = 3;
+
+   System.out.println("a=2");
+   System.out.println("b=4");
+   System.out.println("x=6");
+   System.out.println("y=3");
+
+   if(a == x && y + y == a){
+System.out.println( " ")
+
+
+
+
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
 }
