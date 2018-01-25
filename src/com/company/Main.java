ccf80472f6e51e7ba67ca1ec7df293fa0593b501
Valin Short <valin.short1998@gmail.com>
2018-01-25 15:01:39 -0500

    finished projects

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,31 +9,46 @@ public class Main {
    int a =2;
    int b = 4;
    int x = 6;
-   int y = 3;
+   int y = 1;
 
    System.out.println("a=2");
    System.out.println("b=4");
    System.out.println("x=6");
    System.out.println("y=3");
 
-   if(a == x && y + y == a){
-System.out.println( " ")
+   if(a == x && y + y == a) {
+       System.out.println("");
+
+
+//
+       //&& and symbol
+
+       //|| or
+
+       //== equals too// .equals for strings
+
+       //!= not equal too, ! can be used in front of  values to say that its not
+
+       //< less than
+
+       //> greater than
+
+       //
+
+       if (a < b) ;
+       {
+           System.out.println(a < b);
+
+       }
+
+   }
 
 
 
 
 
-    }
 
 
 
 
 
-
-
-
-
-
-
-
-}
